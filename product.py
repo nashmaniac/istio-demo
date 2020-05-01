97792e1b2cd9ba6f32474ab13ec2f1375378d535
Raju Ahmed Shetu <shetu2153@gmail.com>
2020-05-02 03:44:24 +0600

    initial commit

---
+++ product.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+import os, socket
+
+APP_NAME = os.getenv('APP_NAME')
+
+
+@app.route('/')
+def hello_world():
+    title = '%s Home Page'%APP_NAME
+    return jsonify(
+        dict(
+            app=APP_NAME,
+            served_from=socket.gethostname(),
+            title=title,
+            path=request.path,
+        )
+    )
+
+
+@app.route('/latest')
+def about():
+    title = '%s Latest Page'%APP_NAME
+    return jsonify(
+        dict(
+            served_from=socket.gethostname(),
+            title=title,
+            path=request.path,
+            app=APP_NAME
+        )
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
