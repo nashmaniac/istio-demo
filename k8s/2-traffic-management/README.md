251fd22b0fc99ba2e2e95c93b11d7370a02425df
Raju Ahmed Shetu <shetu2153@gmail.com>
2020-05-09 10:08:59 +0600

    updated the 2-traffic-management

---
+++ k8s/2-traffic-management/README.md
@@ -0,0 +1,9 @@
+* RUN 1, 2, 3, 4 and 6 first.
+* Scenario
+  * For Weighted Routing (Canary Release)
+    * Run 5 and 7
+  * For Session Stickiness (Consistent Hashing)
+    * Run 8 and 9
+  * For Dark release
+    * Run 10 and 11
+  
--- user.py
+++ user.py
@@ -4,19 +4,33 @@ app = Flask(__name__)
 import os, socket
 
 APP_NAME = os.getenv('APP_NAME')
+VERSION = os.getenv('VERSION')
 
 
 @app.route('/')
 def hello_world():
-    title = '%s Home Page'%APP_NAME
-    return jsonify(
-        dict(
-            app=APP_NAME,
-            served_from=socket.gethostname(),
-            title=title,
-            path=request.path,
-        )
+    title = '%s Home Page' % APP_NAME
+    print(request.headers)
+    # V1 code start #
+    data = dict(
+        app=APP_NAME,
+        served_from=socket.gethostname(),
+        title=title,
+        path=request.path,
+        version=VERSION
     )
+    # V1 code end #
+    # V2 code start #
+    # data = dict(
+    #     app=APP_NAME,
+    #     title=title,
+    #     path=request.path,
+    #     version=dict(
+    #         tag=VERSION
+    #     )
+    # )
+    # V2 code end #
+    return jsonify(data)
 
 
 @app.route('/about')
